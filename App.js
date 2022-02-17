46f06c2bb412f0484ead418ba2457883efd78040
SamAshworth13 <19025905@stu.mmu.ac.uk>
2022-02-17 12:02:57 +0000

    Added the foundations of the tab navigation

--- App.js
+++ App.js
@@ -1,25 +1,33 @@
 import React, { Component } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import {View} from 'react-native';
 
 import FeedScreen from './components/feed';
 import SignupScreen from './components/signup';
 import LoginScreen from './components/login';
 
+import ProfileScreen from './components/profile';
+import RequestsScreen from './components/requests';
+import SearchScreen from './components/search';
+
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 class App extends Component {
   render(){
     return (
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="Login" component={LoginScreen} />
-          <Stack.Screen name="Feed" component={FeedScreen} options={{
-          headerLeft: null}} />
-          <Stack.Screen name="Signup" component={SignupScreen} />
-          
-        </Stack.Navigator>
-      </NavigationContainer>
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen name="Login" component={LoginScreen} />
+            <Stack.Screen name="Feed" component={FeedScreen} options={{
+            headerLeft: null}} />
+            <Stack.Screen name="Signup" component={SignupScreen} />
+            
+          </Stack.Navigator>
+        </NavigationContainer>
     );
   }
   
---
+++ components/search.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class SearchScreen extends Component {
+    render(){
+        return (
+            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+                <Text>Search</Text>
+            </View>
+        );
+    } 
+}
+
+export default SearchScreen;
